4049a480a56ad5611ed5e69e6e3976a9030f251c
Ori <OriBennett@gmail.com>
2024-01-31 10:00:30 +0200

    simulation works

---
+++ MediaPlayerClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Players;
+Console.WriteLine("Enter Player:");
+var playerName = Console.ReadLine();
+
+IMediaPlayer mediaPlayer = Factory.CreateMediaPlayer(playerName);
+// Use the media players
+mediaPlayer.Play();
+mediaPlayer.Pause();
+mediaPlayer.Stop();
+
+Console.ReadKey();
